f1c0fab90057b88312a0d9d861866c9bfc41a637
Killua <66186709+Killua6916@users.noreply.github.com>
2021-09-06 11:58:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ function iterate(){
     console.log(x)
     document.getElementById("counter").innerHTML = x
 }
+
+let x
+// if the key value pair exists, we set x equal to the stored value
+// else, set x to 0
+if (localStorage.getItem("count")){
+    x = parseInt(localStorage.getItem("count"))
+} else{
+    x = 0
+}
+document.getElementById('btn').addEventListener("click", iterate)
+document.getElementById("counter").innerHTML = x
+function iterate(){
+    x += 1
+    localStorage.setItem("count", x)
+    console.log(x)
+    document.getElementById("counter").innerHTML = x
+}
