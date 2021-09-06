ff310414113e5377f3bc12012fcb635272d44b2e
Killua <66186709+Killua6916@users.noreply.github.com>
2021-09-06 11:59:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
-//script.js
-let x = 0
-document.getElementById("counter").innerHTML = x
-document.getElementById('btn').addEventListener("click", iterate)
-function iterate(){
-    x += 1
-    console.log(x)
-    document.getElementById("counter").innerHTML = x
+//initial number of cookies    
+var num = 0;
+
+window.onload = function () {
+        var name = prompt("What is your name");
+        
+        var space = document.getElementById("space");
+        
+        space.innerHTML = name + "'s Bakery";
 }
 
-let x
-// if the key value pair exists, we set x equal to the stored value
-// else, set x to 0
-if (localStorage.getItem("count")){
-    x = parseInt(localStorage.getItem("count"))
-} else{
-    x = 0
-}
-document.getElementById('btn').addEventListener("click", iterate)
-document.getElementById("counter").innerHTML = x
-function iterate(){
-    x += 1
-    localStorage.setItem("count", x)
-    console.log(x)
-    document.getElementById("counter").innerHTML = x
+var cookie = document.getElementById("cookie");
+
+function cookieClick() { 
+    num += 1;
+
+    var numbers = document.getElementById("numbers");
+    
+    //upgrade level for printing
+    var upgradeLevel = document.getElementById("upgradeLevel");
+    
+    numbers.innerHTML = num;      
+    //automatic Granny upgrade to 2x
+    if(num >= 30 ){
+        num += 2;
+        upgradeLevel.innerHTML = "Granny Level";
+    }
+
+    //automatic factory upgrade to 10x
+    if(num >= 500) {
+        num += 10;
+        upgradeLevel.innerHTML = "Factory Level";
+    }
+
+    //automatic plant upgrade to 30x
+    if(num >= 1000) {
+        num += 30;
+        upgradeLevel.innerHTML = "Plant Level";
+    }
+
+    //automatic super factory upgrade to 1000x
+    if(num >= 100000) {
+        num += 1000;
+        upgradeLevel.innerHTML = "Super-Plant Level";
+    }
 }
