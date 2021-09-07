2a68cb86231f5dd2a5cd9a0f67fb14da8514bb10
Killua <66186709+Killua6916@users.noreply.github.com>
2021-09-07 11:46:21 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -4,3 +4,15 @@ function findPokeballs() {
   pokeballs = pokeballs + 1;
    quantity.innerHTML = pokeballs;
 }
+
+/* Save Game */
+var save = {
+ pokeballs: pokeballs,
+ quantity: quantity
+}
+localStorage.setItem("save",JSON.stringify(save));
+
+/* Load Game */
+var saveGame = JSON.parse(localStorage.getItem("save"));
+if (typeof saveGame.pokeballs !== "undefined") pokeballs = saveGame.pokeballs;
+
