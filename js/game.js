5f403799fc9ea35549d9c10236fad23f5a5faf47
Killua <66186709+Killua6916@users.noreply.github.com>
2021-09-07 12:05:47 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,21 +1,17 @@
-var pokeballs = 0;
-
-function findPokeballs() {
-  pokeballs = pokeballs + 1;
-  document.getElementById("pokeballs").innerHTML = pokeballs;
+let pokeballs
+// if the key value pair exists, we set x equal to the stored value
+// else, set x to 0
+if (localStorage.getItem("count")){
+    pokeballs = parseInt(localStorage.getItem("count"))
+} else{
+    pokeballs = 0
 }
-
-/* Save Game */
-var save = {
- pokeballs: pokeballs
-}
-
-function save() {
-  localStorage.setItem('pokeballs', JSON.stringify(pokeballs));
-}
-
-/* Load Game */
-function loadGame() {
-  pokeballs = JSON.parse(localStorage.getItem('pokeballs'));
+document.getElementById('pokeballs').addEventListener("click", iterate)
+document.getElementById("counter").innerHTML = pokeballs
+function findPokeballs(){
+    x += 1
+    localStorage.setItem("pokeballs", x)
+    console.log(x)
+    document.getElementById("counter").innerHTML = x
 }
 
